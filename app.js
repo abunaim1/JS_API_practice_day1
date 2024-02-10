4262a524ad48bf1f44c2a3a9fe2b529eddb3b0a8
Abu Naim <abunaim@Abus-MacBook-Air-2.local>
2024-02-10 16:28:03 +0600

    product design done

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const loadCategory = () => {
     .then((data) => displayCategory(data));
 };
 const displayCategory = (category) => {
-//   console.log(category);
+  //   console.log(category);
   const parent = document.getElementById("category");
   category.forEach((item) => {
     const div = document.createElement("div");
@@ -22,21 +22,22 @@ const loadProducts = () => {
 };
 
 const displayProducts = (products) => {
-    const parent = document.getElementById("products")
-    products.forEach(item => {
-        const div = document.createElement("div")
-        div.classList.add("product")
-        div.innerHTML = `
-            <h3>${item.title.slice(0,50)}</h3>
-            <h4>${item.price}</h4>
-            <p class="btn btn-danger">${item.category}</p>
-            <p>${item.description.slice(0,100)}</p>
+  const parent = document.getElementById("products");
+  products.forEach((item) => {
+    const div = document.createElement("div");
+    div.classList.add("product");
+    div.innerHTML = `
+            <h5>${item.title.slice(0, 50)}</h5>
+            <p class="fst-italic bg-success text-light w-50 ps-2">Rating: ${item.rating.rate}</p>
+            <h2>${item.price}$</h2>
+            <p class="btn btn-outline-dark btn-sm">${item.category}</p>
+            <p class="fw-light">${item.description.slice(0, 100)}</p>
             <img src="${item.image}" alt="product image">
             <p></p>
-        `
-        console.log(item);
-        parent.appendChild(div)
-    })
-}
+        `;
+    console.log(item);
+    parent.appendChild(div);
+  });
+};
 loadProducts();
 loadCategory();
